e4bbd7fb7d582bbd4b8bdedd5272585573904ba9
Fearless <fearless@Fearless.fearless.org>
2026-08-18 18:53:27 +0200

    Système de mise à jour complet avec détection de module installé côté clien

---
+++ addons/lometa.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Se dÃ©placer sur le bon lecteur (au cas oÃ¹ vous Ã©tiez sur D: par exemple)
+cd /d "C:\Users\DELL\OneDrive\Documents\Assurance"
+
+:: Lancer le script Python
+python main.py
+
+:: Laisser la fenÃªtre ouverte pour lire les erreurs ou les messages (Optionnel)
+pause
